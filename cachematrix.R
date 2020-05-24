0e9e89749c35793bbcb087b128150e2a2a425d70
elitaylor94 <65119569+elitaylor94@users.noreply.github.com>
2020-05-24 14:49:53 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  
+  # create the matrix in the working environment
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  
+  # get the value of the matrix
+  get <- function() x
+  # invert the matrix and store in cache
+  setMatrix <- function(inverse) 
+    cache <<- inverse
+  # get the inverted matrix from cache
+  getInverse <- function() cache
+  
+  # return the created functions to the working environment as a list
+  list(set = set, #gives the name 'set' to the set() function above
+       get = get, #gives the name 'get' 'to the get() function above
+       setMatrix = setMatrix, #gives the name 'setMatrix' to the setMatrix() function defined above
+       getInverse = getInverse) #gives the name 'getInverse' to the getInverse() function defined above
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## attempt to get the inverse of the matrix stored in cache
+  cache <- x$getInverse()
+  
+  # return inverted matrix from cache if it exists
+  # else create the matrix in working environment
+  if (!is.null(cache)) {
+    message("getting cached data")
+    return(cache)
+  }
+
+  matrix <- x$get()
+  # set and return inverse of matrix
+    cache <- solve(matrix, ...)
+    x$setMatrix(cache)
+  
+  # display matrix in console
+  return (cache)
 }
